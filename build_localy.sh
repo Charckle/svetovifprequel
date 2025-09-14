c7eeb6d1eefc3ccd852cd0edafb575783f34c790
charckle <charckle@email.com>
2025-09-14 21:48:05 +0200

    added local build script 2

--- build_localy.sh
+++ build_localy.sh
@@ -2,4 +2,4 @@ USERNAME=charckle
 VERSION=$(cat VERSION)
 IMAGE=svetovid
 
-docker build -f Dockerfile-alpine -t $USERNAME/$IMAGE:$version-alpine .
+docker build -f Dockerfile-alpine -t $USERNAME/$IMAGE:$VERSION-alpine .
